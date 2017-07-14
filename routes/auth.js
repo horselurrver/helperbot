2471ecbd1511a422af0c2c92211aa2d60d6755d6
Amy Wang <horselurrver@gmail.com>
2017-07-14 14:55:45 -0700

    Tried to fix conflicts

--- routes/auth.js
+++ routes/auth.js
@@ -16,19 +16,10 @@ module.exports = function(passport) {
   // path to start the OAuth flow
   router.get('/auth/slack', passport.authenticate('slack'));
 
-<<<<<<< HEAD
   // OAuth callback url
   router.get('/auth/slack/callback',
     passport.authenticate('slack', { failureRedirect: '/login' }),
     function(req, res){
-=======
-  router.get('/auth/slack', passport.authenticate('slack'));
-  router.get('/auth/slack/callback',
-    passport.authenticate('slack', {
-    failureRedirect: '/login' }),
-    function(req, res) {
-      // Successful authentication, redirect home.
->>>>>>> 44601d0f569ce8e9b94a6bfffc48623514c558c2
       res.redirect('/');
     }
   );
@@ -37,15 +28,9 @@ module.exports = function(passport) {
     res.render('login');
   })
 
-<<<<<<< HEAD
-  // router.get('/index', function(req, res) {
-  //   res.render('index');
-  // })
-=======
   router.get('/index', function(req, res) {
     res.render('index');
   });
->>>>>>> 44601d0f569ce8e9b94a6bfffc48623514c558c2
 
 
   return router;
