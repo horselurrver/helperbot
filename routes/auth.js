15374f21e447cd5133976aa5401ea533f0729980
tlnguyen4 <tlnguyen@princeton.edu>
2017-07-13 22:44:47 -0700

    Additional set up

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var validator = require('express-validator');
+var models = require('../models/models.js');
+
+module.exports = function(passport) {
+
+  return router;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/models')
+var Product = models.Product;
+var mongoose = require('mongoose');
+var products = require('../seed/products');
+mongoose.Promise = global.Promise;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
