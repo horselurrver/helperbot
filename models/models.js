9dd27baaf89fa1fcbcf61df930b24d8d39597767
tlnguyen4 <tlnguyen@princeton.edu>
2017-07-14 12:14:31 -0700

    adding models

--- models/models.js
+++ models/models.js
@@ -1,6 +1,29 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-module.exports = {
+var studentSchema = new Schema({
+  displayName: String,
+  assignedTA: {
+    type: Schema.Types.ObjectId,
+    ref: 'TA'
+  },
+  priority: Number,
+  lastCancel: Date
+});
 
+var taSchema = new Schema({
+  displayName: String,
+  available: Boolean,
+  assignedTo: {
+    type: Schema.Types.ObjectId,
+    ref: 'Student'
+  }
+})
+
+var Student = mongoose.model("Student", studentSchema);
+var Ta = mongoose.model("TA", taSchema);
+
+module.exports = {
+  Student: Student;
+  Ta: Ta;
 }
