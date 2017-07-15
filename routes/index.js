661ba728a949f99f22d01363b816b1ca810321cc
tlnguyen4 <tlnguyen@princeton.edu>
2017-07-14 18:44:34 -0700

    fixing the /get

--- routes/index.js
+++ routes/index.js
@@ -32,51 +32,40 @@ router.get('/index', function(req, res) {
 // this adds the current user to the queue and send back the updated queue
 // check if the current user is first, if so, show the priority button
 router.get('/add', function(req, res) {
-  req.check('category', 'Category is required').notEmpty();
-  var errors = req.validationErrors();
-  if (errors) {
-    res.json(errors);
+  if (req.body.category.length === 0) {
+    res.json("Category is empty");
   } else {
     Student.findByIdAndUpdate(req.user._id, {description: req.body.description, category: req.body.category}, function(error, student) {
       if (queue.length === 0 || queue.length === 1) {
-        queue.push(req.user);
+        queue.push(student);
       } else {
-        // if (student.priority === 3) { // if student has no priority just add to end of queue
-        //   queue.push(req.user);
-        // } else if (student.priority === 2) { // if student is priority 2, find the first person with priority 3 and add them in before this person
-          queue.push(req.user);
-          var key;
-          for (var j = 1; j < queue.length; j++) {
-            key = queue[j].priority;
-            var i = j - 1;
-            while (i >= 1 && queue[i].priority > key) {
-              queue[i + 1] = queue[i];
-              i = i - 1;
-            }
-            queue[i + 1] = key;
+        queue.push(student);
+        var key;
+        for (var j = 1; j < queue.length; j++) {
+          key = queue[j].priority;
+          var i = j - 1;
+          while (i >= 1 && queue[i].priority > key) {
+            queue[i + 1] = queue[i];
+            i = i - 1;
           }
+          queue[i + 1] = key;
         }
+        var isFirst = false;
+        if (queue[0].username === req.user.username) isFirst = true;
+        var returnObj = {
+          queue: queue,
+          isFirst: isFirst
+        }
+        res.json(returnObj);
       }
     });
-    var isFirst = false;
-    if (queue[0].username === req.user.username) isFirst = true;
-    var returnObj = {
-      queue: queue,
-      isFirst: isFirst
-    }
-    res.json(returnObj);
   }
 });
 
 // remove current user from the queue and send back update queue
 // check if current user is first, if so, show the priority button
 router.post('/cancel', function(req, res) {
-  var indexOfCurrentUser = queue.indexOf(req.user);
-  if (indexOfCurrentUser === 0) {
-    Student.findByIdAndUpdate()
-  }
-  // queue.splice(indexOfCurrentUser, 1);
-  // res.json({queue: queue});
+  if ()
 });
 
 //
